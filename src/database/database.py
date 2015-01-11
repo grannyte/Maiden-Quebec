48c478241ee6c5df8718249dd6b094de3707e19c
Pierre-Luc Perron <pl.perron@hotmail.com>
2015-01-10 19:10:47 -0500

    Ajouter des armes dans la base de données

--- src/database/database.py
+++ src/database/database.py
@@ -15,3 +15,12 @@ class Database:
 
     def __exit__(self, type, value, traceback):
         self.conn.close()
+
+    def populate(self):
+        """Inscrit des éléments à l'intérieur de la base de données"""
+        self.cursor.execute('CREATE TABLE weapons (name, damage)')
+        weapons = [('excalibur, 10'),
+                   ('Ragnarok', '5'),
+                   ('dague rouillé', '1'),
+                   ]
+        self.cursor.executemany('INSERT INTO weapons VALUES (?,?)', weapons)
