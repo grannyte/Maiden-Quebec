b91d232a19a16892960db9770b0cdeb79eb95bbe
Pierre-Luc Perron <pl.perron@hotmail.com>
2015-01-11 20:30:31 -0500

    Ajouter un serveur

--- src/main.py
+++ src/main.py
@@ -1,12 +1,15 @@
 # -*- coding: utf-8 -*-
 
+import argparse
+import src.serveur as serveur
+import socketserver
+
 
 def main():
     """
     Point d'entré du programme.  Celui-ci valide que les arguments passés en paramètre
     sont conformes.
     """
-    import argparse
 
     parser = argparse.ArgumentParser(description='Text-based game')
     parser.add_argument('-i', '--irrlicht',
@@ -15,5 +18,17 @@ def main():
     parser.parse_args()
 
 
+def run_server():
+    """Démarre le serveur"""
+    host, port = "localhost", 9999
+
+    # Create the server, binding to localhost on port 9999
+    server = socketserver.TCPServer((host, port), serveur.Server)
+
+    # Activate the server; this will keep running until you
+    # interrupt the program with Ctrl-C
+    server.serve_forever()
+
 if __name__ == "__main__":
     main()
+    run_server()
