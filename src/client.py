7fb709872fad6b26dde9e48e2ed456de3a7eaeca
Pierre-Luc Perron <pl.perron@hotmail.com>
2015-01-15 20:06:32 -0500

    Ajouter l'option de quitter l'interface texte

--- src/client.py
+++ src/client.py
@@ -1,37 +1,17 @@
-# -*- coding: utf-8 -*-
-
-import socket
-import sys
+import curses
 
 
-class Client():
+def main(stdscr):
+    stdscr.nodelay(1)
+    curses.noecho()
+    curses.cbreak()
+    curses.endwin()
+    curses.curs_set(False)
+    stdscr.keypad(True)
 
-    def __init__(self, host, port):
-        self.host, self.port = host, port
+    while True:
+        if stdscr.getch() == ord('q'):
+            break
 
-    def run(self):
-        data = " ".join(sys.argv[1:])
-
-        # Create a socket (SOCK_STREAM means a TCP socket)
-        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-        try:
-            # Connect to server and send data
-            sock.connect((self.host, self.port))
-            sock.sendall(bytes(data + "\n", "utf-8"))
-
-            # Receive data from the server and shut down
-            received = str(sock.recv(1024), "utf-8")
-        finally:
-            sock.close()
-
-        print("Sent:     {}".format(data))
-        print("Received: {}".format(received))
-
-
-def run_client():
-    c = Client("localhost", 9999)
-    c.run()
-
-if __name__ == "__main__":
-    run_client()
+if __name__ == '__main__':
+    curses.wrapper(main)
