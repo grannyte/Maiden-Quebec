cb025053fb4cf9a3d633b1b47578bc3778cb2c01
Jean-Marc Jodoin <jmjodoin@hotmail.com>
2015-01-17 09:29:21 -0500

    ajout - getters pour Classe Map
    Avec les tests. Et correction du caractere de changement de ligne de la
    map.

--- src/map/loader.py
+++ src/map/loader.py
@@ -4,8 +4,23 @@ class Map:
     map = None
 
     def __init__(self, filename):
-        self.map = [list(line) for line in open(filename)]    
+        self.map = [list(line[:-1]) for line in open(filename)]
+
+    def getNumberOfRows(self):
+        return len(self.map)
+
+    def getNumberOfColumns(self):
+        return len(self.map[0])
+
+    def getElement(self,x,y):
+        return self.map[x][y]
+
+    def getPositionElement(self,element):
+        for row in range(len(self.map)):
+            for col in range(len(self.map[0])):
+                if(self.map[row][col] == element):
+                    return row, col
 
     def __str__(self):
         return ''.join(str(item) for line in self.map for item in line)
-     
+
--- test/map/loader_test.py
+++ test/map/loader_test.py
@@ -31,13 +31,43 @@ class MapLoaderTestCase(unittest.TestCase):
     def test_repr(self):
         filename= os.path.abspath(os.path.join(self.data_dir, "level00.map"))
         actual = str(Map(filename))
-        expect = ("#######\n"
-                  "#     #\n"
-                  "# H  *#\n"
-                  "#  ++$#\n"
+        expect = ("#######"
+                  "#     #"
+                  "# H  *#"
+                  "#  ++$#"
                   "#######"
         )
         self.assertEqual(expect, actual)
+
+    def test_getNumberOfRows(self):
+        filename= os.path.abspath(os.path.join(self.data_dir, "level00.map"))
+        map = Map(filename)
+        actual = map.getNumberOfRows()
+        expect = 5
+        self.assertEqual(expect, actual)
+
+    def test_getNumberOfColumns(self):
+        filename= os.path.abspath(os.path.join(self.data_dir, "level00.map"))
+        map = Map(filename)
+        actual = map.getNumberOfColumns()
+        expect = 7
+        self.assertEqual(expect, actual)
+
+    def test_getElement(self):
+        filename= os.path.abspath(os.path.join(self.data_dir, "level00.map"))
+        map = Map(filename)
+        actual = map.getElement(2,2)
+        expect = 'H'
+        self.assertEqual(expect, actual)
+
+    def test_getPositionElement(self):
+        filename= os.path.abspath(os.path.join(self.data_dir, "level00.map"))
+        map = Map(filename)
+        actual_row, actual_col = map.getPositionElement('$')
+        expect_row = 3
+        expect_col = 5
+        self.assertEqual(expect_row, actual_row)
+        self.assertEqual(expect_col, actual_col)
 
 if __name__ == '__main__':
     unittest.main()
