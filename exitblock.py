a0410540ce5fb6f5085362694dc965562009f89b
granyte <granyte@hotmail.com>
2015-01-21 05:30:01 -0500

    Gestion des differents type de cubes

--- exitblock.py
+++ exitblock.py
@@ -8,4 +8,17 @@ from platform import Platform
 class ExitBlock(Platform):
     def __init__(self, x, y):
         Platform.__init__(self, x, y)
-        self.image.fill(Color("#0033FF"))
+        rect = (32*6, 2*32, 32, 32)
+        self.image = self._spritesheet.image_at(rect)
+
+    def _init_project_dir(self):
+        """Initialize project directory"""
+        full_path = os.path.realpath(__file__)
+        self._project_dir = os.path.dirname(full_path)
+
+    def _init_spritesheet(self):
+        """Initialize spritesheet"""
+        spritesheets = ["fantasy-tileset.png"]
+        ss_path = os.path.join('', *[self._project_dir, 'data', spritesheets[0]])
+        self._spritesheet = spritesheet.Spritesheet(ss_path)
+
