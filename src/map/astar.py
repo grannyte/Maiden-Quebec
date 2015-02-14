ea1acc9f2af60e81965075df0624c5e5652d93c7
Jean-Marc Jodoin <jmjodoin@hotmail.com>
2015-02-14 13:11:13 -0500

    ajout - A* event driven

--- src/map/astar.py
+++ src/map/astar.py
@@ -1,58 +1,71 @@
+from map import Map
 import heapq
+
+def main():
+    map = Map('test10.map')
+    map.print_as_grid()
+    world = World(map.grid)
+    initial_state = State(Cell(1,1,True))
+    goal = Goal(Cell(13,39,True))
+    heuristic = Heuristic()
+    astar = AStar(world, initial_state, goal, heuristic)
+
+    final_state = astar.final_state
+    if(final_state != None):
+        print("found")
+        while(final_state != None):
+            print("Cell: " + str(final_state.cell.x) + "," + str(final_state.cell.y))
+            final_state = final_state.parent
+    else:
+        print("not found")
 
 class Cell(object):
     def __init__(self, x, y, reachable):
         self.reachable = reachable
         self.x = x
         self.y = y
+
+    def __eq__(self, othercell):
+        return self.x == othercell.x and self.y == othercell.y
+
+class Action(object):
+    def __init__(self, cost, cell):
+        self.cost = cost
+        self.cell = cell
+
+class Goal(object):
+    def __init__(self, cell):
+        self.cell = cell
+
+    def is_reached(self, state):
+        return self.cell == state.cell
+
+class Heuristic(object):
+    def __init__(self):
+        pass
+
+    def estimate_cost_to_goal(self, state, goal):
+        return 10 * (abs(goal.cell.x - state.cell.x) + abs(goal.cell.y - state.cell.y))
+
+class State(object):
+    def __init__(self, cell):
+        self.cell = cell
         self.parent = None
+        self.action_since_parent = None
+        self.f = 0
         self.g = 0
         self.h = 0
-        self.f = 0
 
-    def __lt__(self, other):
-        return self.f < other.f
+    def __eq__(self, otherstate):
+        if(otherstate != None):
+            return self.cell.x == otherstate.cell.x and self.cell.y == otherstate.cell.y
+        else:
+            return None
 
+    def __lt__(self, otherstate):
+        return self.f < otherstate.f
 
-class AStar(object):
-    def process(self):
-        self.opened = []
-        self.closed = set()
-
-        # add start cell to heap queue
-        heapq.heapify(self.opened)
-        heapq.heappush(self.opened, (self.start.f, self.start))
-
-        while len(self.opened):
-            # pop cell from heap queue
-            f, cell = heapq.heappop(self.opened)
-            # add cell to closed list
-            self.closed.add(cell)
-            # if end cell, display found path
-            if cell is self.end:
-                self.path_found = True
-                break
-            # get adjacent cells for cell
-            adj_cells = self.get_adjacent_cells(cell)
-            for adj_cell in adj_cells:
-                if adj_cell.reachable and adj_cell not in self.closed:
-                    if (adj_cell.f, adj_cell) in self.opened:
-                        # if adj cell in open list, check if current path is better than the one previously found for this adj cell.
-                        if adj_cell.g > cell.g + self.get_heuristic_base_value():
-                            self.update_cell(adj_cell, cell)
-                    else:
-                        self.update_cell(adj_cell, cell)
-                        # add adj cell to open list
-                        heapq.heappush(self.opened, (adj_cell.f, adj_cell))
-
-    def update_cell(self, adj, cell):
-        adj.g = cell.g + self.get_heuristic_base_value()
-        adj.h = self.get_heuristic(adj)
-        adj.parent = cell
-        adj.f = adj.h + adj.g
-
-
-class Search(AStar):
+class World(object):
     def __init__(self, grid):
         self.cells = []
         self.grid_def = {'start':'H', 'end':'E', 'wall':'#'}
@@ -75,59 +88,62 @@ class Search(AStar):
                 elif(current_element == self.grid_def['end']):
                     self.end = self.get_cell(x, y)
 
-    def get_heuristic(self, cell):
-        return self.get_heuristic_base_value() * (abs(cell.x - self.end.x) + abs(cell.y - self.end.y))
+    def getActions(self, state):
+        actions = []
+        if state.cell.x < self.grid_width-1 and self.get_cell(state.cell.x+1, state.cell.y).reachable == True:
+            actions.append(Action(10,self.get_cell(state.cell.x+1, state.cell.y)))
+        if state.cell.y > 0 and self.get_cell(state.cell.x, state.cell.y-1).reachable == True:
+            actions.append(Action(10,self.get_cell(state.cell.x, state.cell.y-1)))
+        if state.cell.x > 0 and self.get_cell(state.cell.x-1, state.cell.y).reachable == True:
+            actions.append(Action(10,self.get_cell(state.cell.x-1, state.cell.y)))
+        if state.cell.y < self.grid_height-1 and self.get_cell(state.cell.x, state.cell.y+1).reachable == True:
+            actions.append(Action(10,self.get_cell(state.cell.x, state.cell.y+1)))
+        return actions
 
-    def get_heuristic_base_value(self):
-        return 10
+    def execute(self, etat, action):
+        return State(Cell(action.cell.x, action.cell.y, True))
 
     def get_cell(self, x, y):
         return self.cells[x * self.grid_height + y]
 
-    def get_adjacent_cells(self, cell):
-        cells = []
-        if cell.x < self.grid_width-1:
-            cells.append(self.get_cell(cell.x+1, cell.y))
-        if cell.y > 0:
-            cells.append(self.get_cell(cell.x, cell.y-1))
-        if cell.x > 0:
-            cells.append(self.get_cell(cell.x-1, cell.y))
-        if cell.y < self.grid_height-1:
-            cells.append(self.get_cell(cell.x, cell.y+1))
-        return cells
 
-    def display_path(self):
-        if(self.path_found):
-            cell = self.end
-            print("path: cell: ",cell.x,", ", cell.y)
-            while cell.parent is not self.start:
-                cell = cell.parent
-                print("path: cell: ",cell.x,", ", cell.y)
-            cell = self.start
-            print("path: cell: ",cell.x,", ", cell.y)
+class AStar(object):
+    def __init__(self, world, initialState, goal, heuristic):
+        self.opened = []
+        self.closed = []
+        self.final_state = None
 
-    def display_path_as_grid(self):
-        path = self.get_path()
-        for x in range(self.grid_width):
-            for y in range(self.grid_height):
-                cell = self.get_cell(x,y)
-                if((x,y) in path):
-                    print("o", end="")
-                elif(cell.reachable):
-                    print(" ", end="")
-                else:
-                    print("#", end="")
-            print()
+        # add the first state to the heap
+        heapq.heapify(self.opened)
+        heapq.heappush(self.opened, (initialState.f, initialState))
 
-    def get_path(self):
-        cells = []
-        if(self.path_found):
-            cell = self.end
-            cells.append((cell.x, cell.y))
-            while cell.parent is not self.start:
-                cell = cell.parent
-                cells.append((cell.x, cell.y))
-            cell = self.start
-            cells.append((cell.x, cell.y))
-        return cells
+        while len(self.opened):
+            # pop state from heap queue
+            f, actual_state = heapq.heappop(self.opened)
+            print("POP: " + str(actual_state.f) + " " + str(actual_state.cell.x) + " " + str(actual_state.cell.y))
+            # add state to closed list
+            self.closed.append(actual_state)
 
+            # if goal is reached, stop
+            if goal.is_reached(actual_state):
+                self.final_state = actual_state
+                break
+
+            # get possible actions from current state
+            possible_actions = world.getActions(actual_state)
+
+            for action in possible_actions:
+                next_state = world.execute(actual_state, action)
+                next_state.action_since_parent = action
+                next_state.g = actual_state.g + action.cost
+                next_state.h = heuristic.estimate_cost_to_goal(next_state, goal)
+                next_state.f = next_state.g + next_state.h
+                next_state.parent = actual_state
+
+                if(next_state not in self.closed):
+                    if((next_state.f, next_state) not in self.opened):
+                        heapq.heappush(self.opened, (next_state.f, next_state))
+
+
+if __name__ == "__main__":
+    main()
