a1bb5bafbbfc8688fab2908c3b79c2e8392b5454
Jean-Marc Jodoin <jmjodoin@hotmail.com>
2015-03-11 17:50:08 -0400

    Ajouter monstre et configurer map pour librpg

--- worldtest/mymaps.py
+++ worldtest/mymaps.py
@@ -1,8 +1,10 @@
+# -*- coding: utf-8 -*-
+
 from librpg.world import WorldMap
 from librpg.mapobject import ScenarioMapObject, MapObject
 from librpg.maparea import RectangleArea, MapArea
 from librpg.util import Position
-from librpg.movement import Face, Wait
+from librpg.movement import Face, Wait, ForcedStep
 from librpg.dialog import MessageDialog
 from librpg.locals import *
 from librpg.path import *
@@ -13,22 +15,32 @@ LOWER_TILESET = (tileset_path('city_lower.png'),
                  tileset_path('city_lower.bnd'))
 UPPER_TILESET = [(tileset_path('world_upper.png'),
                   tileset_path('world_upper.bnd'))]
+HP_INITIAL = 100
+HAUT_TOUR = 12
+BAS_TOUR = 22
 
 class SavePoint(ScenarioMapObject):
-
     def __init__(self, map):
         ScenarioMapObject.__init__(self, map, 0, 1)
+        self.map = map
 
     def activate(self, party_avatar, direction):
-        self.map.schedule_message(MessageDialog('You game will be saved to %s.'
+        self.map.schedule_message(MessageDialog(u'Votre partie se sauvegarge dans %s.'
                                   % SAVE_FILE, block_movement=True))
         self.map.save_world(SAVE_FILE)
-        self.map.schedule_message(MessageDialog('Game saved.',
+        self.map.schedule_message(MessageDialog(u'Partie sauvegardée.',
                                                 block_movement=True))
 
+class MessagePoint(ScenarioMapObject):
+    def __init__(self, map, position, message):
+        ScenarioMapObject.__init__(self, map, 0, position)
+        self.map = map
+        self.message = message
+
+    def activate(self, party_avatar, direction):
+        self.map.schedule_message(MessageDialog(self.message, block_movement=True))
 
 class Chest(MapObject):
-
     def __init__(self, closed=True):
         MapObject.__init__(self, MapObject.OBSTACLE,
                            image_file=charset_path('chest.png'),
@@ -50,33 +62,32 @@ class Chest(MapObject):
         else:
             self.schedule_movement(Face(UP))
             self.closed = True
+        print u'Bravo vous avez complété le jeu.'
+        self.map.gameover()
 
 
-class Map1(WorldMap):
-
+class Monster(MapObject):
     def __init__(self):
-        WorldMap.__init__(self, 'worldtest/map1.map',
-                          LOWER_TILESET,
-                          UPPER_TILESET)
+        MapObject.__init__(self, MapObject.OBSTACLE,
+                           image_file='hulk.png')
+        self.movement_behavior.movements.extend([Wait(30), ForcedStep(LEFT),
+                                                 Wait(30), ForcedStep(RIGHT)])
+        self.hp = HP_INITIAL
 
-    def initialize(self, local_state, global_state):
-        self.add_area(RelativeTeleportArea(x_offset=-8, map_id=2),
-                      RectangleArea((9, 0), (9, 9)))
+    def activate(self, party_avatar, direction):
+        self.hp -= 10
+        print(u'Attaque du monstre (-10) [' + str(self.hp) + '/' + str(HP_INITIAL) + ']')
+        if(self.hp <= 0):
+            print(u'Le monstre est mort.')
+            self.destroy()
 
-        if local_state is not None:
-            self.chest = Chest(local_state['chest_closed'])
-        else:
-            self.chest = Chest()
-        self.add_object(self.chest, Position(5, 5))
+    def collide_with_party(self, party_avatar, direction):
+        print('defense')
 
-        self.add_object(SavePoint(self), Position(6, 7))
-
-    def save_state(self):
-        return {'chest_closed': self.chest.closed}
-
+    def update(self):
+        pass
 
 class AreaAroundWell(MapArea):
-
     def party_entered(self, party_avatar, position):
         print 'party_entered(%s, %s)' % (party_avatar, position)
 
@@ -89,8 +100,27 @@ class AreaAroundWell(MapArea):
         print 'party_left(%s, %s)' % (party_avatar, position)
 
 
-class Map2(WorldMap):
+class GameOverBarrel(ScenarioMapObject):
+    def __init__(self, map):
+        ScenarioMapObject.__init__(self, map, 0, 4)
 
+    def activate(self, party_avatar, direction):
+        print u'Le barril explose et vous êtes mort.'
+        self.map.gameover()
+
+
+class Map1(WorldMap):
+    def __init__(self):
+        WorldMap.__init__(self, 'worldtest/map1.map',
+                          LOWER_TILESET,
+                          UPPER_TILESET)
+
+    def initialize(self, local_state, global_state):
+        self.add_area(RelativeTeleportArea(x_offset=-8, map_id=2), RectangleArea((9, 2), (9, 8)))
+        self.add_object(MessagePoint(self,HAUT_TOUR,u'Vieil homme dans la tour: Vous aviez bu trop de Maiden-Quebec et vous avez coulé votre navire.  Les vagues vous ont ramené sur le rivage, comptez vous chanceux d\'être en vie! Vous devriez retourner à la maison maintenant...'), Position(3, 3))
+        self.add_object(MessagePoint(self,BAS_TOUR,u'Vieil homme dans la tour: Vous aviez bu trop de Maiden-Quebec et vous avez coulé votre navire.  Les vagues vous ont ramené sur le rivage, comptez vous chanceux d\'être en vie! Vous devriez retourner à la maison maintenant...'), Position(3, 4))
+
+class Map2(WorldMap):
     def __init__(self):
         WorldMap.__init__(self, 'worldtest/map2.map',
                           LOWER_TILESET,
@@ -98,51 +128,35 @@ class Map2(WorldMap):
 
     def initialize(self, local_state, global_state):
         self.add_area(RelativeTeleportArea(x_offset=+8, map_id=1),
-                      RectangleArea((0, 0), (0, 9)))
+                      RectangleArea((0, 2), (0, 7)))
 
         self.add_area(RelativeTeleportArea(x_offset=-8, map_id=3),
-                      RectangleArea((9, 0), (9, 9)))
+                      RectangleArea((9, 2), (9, 7)))
 
-        if local_state is not None:
-            self.chest = Chest(local_state['chest_closed'])
-        else:
-            self.chest = Chest()
-        self.add_object(self.chest, Position(5, 5))
-
-        self.add_object(SavePoint(self), Position(3, 4))
-        self.add_area(AreaAroundWell(), RectangleArea((2, 3), (4, 5)))
-
-    def save_state(self):
-        return {'chest_closed': self.chest.closed}
-
-
-class GameOverBarrel(ScenarioMapObject):
-
-    def __init__(self, map):
-        ScenarioMapObject.__init__(self, map, 0, 4)
-
-    def activate(self, party_avatar, direction):
-        print 'The barrel explodes and you die.'
-        self.map.gameover()
+        self.add_object(SavePoint(self), Position(5, 2))
+        #self.add_area(AreaAroundWell(), RectangleArea((2, 3), (4, 5)))
 
 
 class Map3(WorldMap):
-
     def __init__(self):
         WorldMap.__init__(self, 'worldtest/map3.map',
                           LOWER_TILESET,
                           UPPER_TILESET)
 
     def initialize(self, local_state, global_state):
-        self.add_object(GameOverBarrel(self), Position(6, 4))
+        self.add_object(GameOverBarrel(self), Position(9, 3))
+        self.add_object(GameOverBarrel(self), Position(9, 7))
+
+        self.add_object(Monster(), Position(8,5))
 
         self.add_area(RelativeTeleportArea(x_offset=+8, map_id=2),
-                      RectangleArea((0, 0), (0, 9)))
+                      RectangleArea((0, 3), (0, 6)))
 
-        self.add_area(RelativeTeleportArea(y_offset=+8),
-                      RectangleArea((0, 0), (9, 0)))
-        self.add_area(RelativeTeleportArea(y_offset=-8),
-                      RectangleArea((0, 9), (9, 9)))
+        if local_state is not None:
+            self.chest = Chest(local_state['chest_closed'])
+        else:
+            self.chest = Chest()
+        self.add_object(self.chest, Position(9, 5))
 
-    def custom_gameover(self):
-        print 'Map3.custom_gameover()'
+    def save_state(self):
+        return {'chest_closed': self.chest.closed}
--- worldtest/myworld.py
+++ worldtest/myworld.py
@@ -6,12 +6,12 @@ from librpg.path import *
 from worldtest.mymaps import *
 
 def char_factory(name):
-    return Character('Andy', charset_path('naked_man.png'))
+    return Character('Andy', charset_path('player.png'))
 
 class MyWorld(World):
-
     def __init__(self, save_file=None):
         maps = {1: Map1, 2: Map2, 3: Map3}
+
         World.__init__(self, maps=maps, character_factory=char_factory)
         if save_file is None:
             self.initial_state(map=1, position=Position(5, 4), chars=['Andy'])
@@ -19,4 +19,4 @@ class MyWorld(World):
             self.load_state(state_file=save_file)
 
     def custom_gameover(self):
-        print 'MyWorld.custom_gameover()'
+        print 'Fin du jeu.'
